5bee0de694bbebdf54fb3b4182e2cac420f22a48
Nathan Zukoff <nzukoff@gmail.com>
2017-10-16 21:20:21 -0700

    starfield moves with cursor, mousepressed is choppy

--- starfield/sketch.js
+++ starfield/sketch.js
@@ -1,20 +1,51 @@
 let stars = []
+let x = 0, y = 0, scaleAmount = 1
 
 function setup() {
 	canvas = createCanvas(windowWidth,windowHeight);
 	// let x = (windowWidth - width) / 2;
 	// let y = (windowHeight - height) / 2
 	// canvas.position(x, y);
-	for (i = 0; i < 20; i++) {
+	for (i = 0; i < 200; i++) {
 		stars.push(new Star())
 	}
 }
 
+function keyPressed() {
+	// let inc = 2
+	// if (keyCode === LEFT_ARROW) {
+	// 	console.log("L")
+	// 	x -= inc
+	// } else if (keyCode === RIGHT_ARROW) {
+	// 	  console.log("R")
+	// 	x += inc	  
+	// } else if (keyCode === UP_ARROW) {
+	// 	console.log("R")
+	//   	y += inc	  
+  	// } else if (keyCode === DOWN_ARROW) {
+	// 	console.log("R")
+	//   	y -= inc	  
+  	// }
+
+	if (keyCode === UP_ARROW) {
+		scaleAmount += 0.05
+	} else if (keyCode === DOWN_ARROW) {
+		scaleAmount -= 0.05
+	}
+  }
+
 function draw() {
 	background(0)
-	translate(width/2, height/2)
-	for (i = 0; i < 20; i++) {
+	let moveX = map(mouseX, 0, width, -width/2, width/2)
+	let moveY = map(mouseY, 0, height, -height/2, height/2)
+	// translate(width/2+x, height/2+y)
+	translate(width/2+moveX, height/2+moveY)
+	
+	// var scaleAmount = mouseX / 200;
+	scale(scaleAmount);
+	for (i = 0; i < stars.length; i++) {
 		stars[i].update()
 		stars[i].show()
 	}
+	
 }
--- starfield/star.js
+++ starfield/star.js
@@ -3,15 +3,16 @@ class Star {
         this.x = random(-width/2,width/2)
         this.y = random(-height/2,height/2)
         this.z = random(width)
-        this.px = this.x
+        this.pz = this.z
     }
 
     update() {
-        this.z -= 1
+        this.z -= 20
         if (this.z < 1) {
             this.x = random(-width/2,width/2)
             this.y = random(-height/2,height/2)
             this.z = random(width)
+            this.pz = this.z
         }
     }
 
@@ -21,6 +22,11 @@ class Star {
         let sx = map(this.x/this.z, 0, 1, 0, width)
         let sy = map(this.y/this.z, 0, 1, 0, height)
         let r = map(this.z, 0, width, 16, 0)
-        ellipse(sx,sy,r)
+        // ellipse(sx,sy,r)
+        let px = map(this.x/this.pz, 0, 1, 0, width)
+        let py = map(this.y/this.pz, 0, 1, 0, height)
+        this.pz = this.z
+        stroke(255)
+        line(px, py, sx, sy)
     }
 }
